3325c2b04e2300ff8d6d3821912fe4f8cda28784
VirtualVasquez <mvasq95@hotmail.com>
2021-05-20 16:03:04 -0400

    default ten characters appear correctly. figure out search function

--- src/App.js
+++ src/App.js
@@ -4,24 +4,12 @@ import React from 'react';
 
 //IN PROGRESS============
 // Deliverables
-  // The home page should have a table of star wars character
-  // Each row should contain the character’s:
-      // Name
-      // Birth date
-      // Height
-      // Mass
-      // Homeworld
-      // Species
-  // The table should automatically populate when opening the home page
   // The table should be limit results to 10 characters at a time
+    //implying you should be able to more, but 10 at a time. 
   // There should be a single input element for searching a specific user
 
-//DONE=================
-
-  //http://swapi.dev/api/people/?search=NAME === search on partial query, but need to learn to refine.
-  //http://swapi.dev/api/people === everybody, but returns only 10
-  //http://swapi.dev/api/people/?page={$'integer'} === one of nine pages of characters
 let API = "http://swapi.dev/api/people/?page=1"
+
 
 
 class App extends React.Component{
@@ -30,21 +18,39 @@ class App extends React.Component{
     this.state = {
       results:[]
     }
+    this.getPlanets = this.getPlanets.bind(this);
+    this.getSpecies = this.getSpecies.bind(this);
+    this.getOtherData = this.getOtherData.bind(this);
+    
   }
-  
+
+ getPlanets = async(character) =>{
+    const planet = character.homeworld;
+    const response = await axios.get(planet);
+    character.homeworld = response.data.name
+  }
+  getSpecies = async (character) =>{
+    if(character.species.length === 0){
+      character.species = "Human";
+    } else {
+      const response = await axios.get(character.species);
+      character.species = response.data.name
+    }
+  }
+  getOtherData = async (characters) => {
+    for (const character of characters) {
+      await this.getPlanets(character);
+      await this.getSpecies(character);
+      this.setState({
+        results:[...characters]
+      })
+    }
+  }
+
   
   componentDidMount() {
     axios.get(API)
-    .then(response => {
-      // console.log(response.data.results);
-      this.setState({
-        results:[...response.data.results]
-      })
-      this.state.results.forEach((char)=>console.log(char.name))
-    })
-    .catch(error => {
-      console.log(error);
-    });
+    .then((response) => this.getOtherData(response.data.results))
   }
 
   render(){
@@ -69,7 +75,21 @@ class App extends React.Component{
               </tr>
             </thead>
             <tbody>
+              {
+                this.state.results.map(function(result){
+                  return(
+                    <tr>
+                      <td>{result.name}</td>
+                      <td>{result.birth_year}</td>
+                      <td>{result.height}</td>
+                      <td>{result.mass}</td>
+                      <td>{result.homeworld}</td>
+                      <td>{result.species}</td>
+                    </tr>
+                  )
+                })
 
+              }
             </tbody>
           </table>
 
